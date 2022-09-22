1b1157b2f3845d763b90cbaef17cf827d43f485c
Emily Raventos <emilyraventos@gmail.com>
2022-09-22 10:53:14 -0600

    cse210 Thursday Notes

--- Program.cs
+++ Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string>();
+            names.Add("Bob");
+            names.Add("Bill");
+            names.Add("Sue");
+            
+            names.ForEach(name => Console.Write(name + " "));
+
+            string a_string = "a";
+            List<string> stuff = new List<string> { a_string, "b", "c", "d"};
+
+            char aSingleCharacter = 'a';
+            Console.WriteLine($"This is a single character: {aSingleCharacter}");
+
+            int x = 5;
+            int y = 7;
+            int result = Adder(x, y); //12
+            
+            int result2 = Adder(10, 46); //56
+
+            Console.WriteLine($"{x} + {y} = {result}");
+        }
+
+        // Create functions. For now start with static
+        static int Adder(int a, int b)
+        {
+            return a + b;
+        }
+
+        // Notes about the tic-tac-toe program:
+        
+    }
+}
